b635812ebb47160642080de5e717cd1b40d65d95
unknown <unknown>
2025-03-05 06:49:44 +0500

    first commit

--- src/utils/fackData/serviceThreeData.js
+++ src/utils/fackData/serviceThreeData.js
@@ -1,33 +1,32 @@
 export const serviceThreeData = [
-    {
-        id: 1,
-        service_name: "Metal Forming",
-        src: '/images/service/s1.jpg'
-    },
-    {
-        id: 2,
-        service_name: "Power & Energy Sector",
-        src: '/images/service/s2.jpg'
-    },
-    {
-        id: 3,
-        service_name: "Petroleum Refinery",
-        src: '/images/service/s3.jpg'
-    },
-    {
-        id: 4,
-        service_name: "Mechanical Engineering",
-        src: '/images/service/s4.jpg'
-    },
-    {
-        id: 5,
-        service_name: "Construction Services",
-        src: '/images/service/s5.jpg'
-    },
-    {
-        id: 6,
-        service_name: "Property Maintenance",
-        src: '/images/service/s6.jpg'
-    }
-]
-
+  {
+    id: 1,
+    service_name: "Excavation",
+    src: "/images/service/s1.jpg",
+  },
+  {
+    id: 2,
+    service_name: "Road Division",
+    src: "/images/service/s2.jpg",
+  },
+  {
+    id: 3,
+    service_name: "Asphalt",
+    src: "/images/service/s3.jpg",
+  },
+  {
+    id: 4,
+    service_name: "Interlock Fixing",
+    src: "/images/service/s4.jpg",
+  },
+  {
+    id: 5,
+    service_name: "Building Materials ",
+    src: "/images/service/s5.jpg",
+  },
+  {
+    id: 6,
+    service_name: "Property Maintenance",
+    src: "/images/service/s6.jpg",
+  },
+];
