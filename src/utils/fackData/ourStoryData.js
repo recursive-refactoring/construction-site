34fc7e81497f64a4ee8c482c3cc7335fc3dc627e
unknown <unknown>
2025-03-05 05:23:28 +0500

    first commit

--- src/utils/fackData/ourStoryData.js
+++ src/utils/fackData/ourStoryData.js
@@ -1,24 +1,26 @@
 export const ourStoryData = [
-    {
-        id: 1,
-        year: "1999-2002",
-        title: "start the company story",
-        src: "/images/story/01.jpg",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly"
-    },
-    {
-        id: 2,
-        year: "1999-2002",
-        title: "start the first destination",
-        src: "/images/story/02.jpg",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly"
-    },
-    {
-        id: 3,
-        year: "1999-2002",
-        title: "#1 Worlds Award winning company",
-        src: "/images/story/03.jpg",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly"
-    },
-
-]
+  {
+    id: 1,
+    year: "",
+    title: "Our Mission",
+    src: "/images/story/01.png",
+    description:
+      "Our mission is to provide superior construction services that enhance infrastructure and communities. We strive for excellence through innovation, sustainability, and efficiency while ensuring safety and client satisfaction.",
+  },
+  {
+    id: 2,
+    year: "",
+    title: "Our Vision",
+    src: "/images/story/02.png",
+    description:
+      "To be a leading construction company recognized for high-quality craftsmanship, cutting-edge solutions, and a commitment to building a better, more sustainable future.",
+  },
+  {
+    id: 3,
+    year: "",
+    title: "Our Values",
+    src: "/images/story/03.png",
+    description:
+      "We uphold integrity by fostering honesty, transparency, and trust in all our operations. Our commitment to excellence ensures superior craftsmanship and outstanding project quality. Prioritizing safety, we create a secure environment for our team, clients, and the communities we serve.",
+  },
+];
